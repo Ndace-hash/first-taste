df1e80faac25ab49c14fd06e29b6d2c5f74b9dff
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-19 12:27:38 +0100

    added a profile page theme with a display picture

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Head } from "@inertiajs/react";
 
 import AppNavbar from "../Components/AppNavbar";
+import passport from "../../images/passport.jpg";
 
 const HomePage = ({ user }) => {
     return (
-        <section className="text-center h-screen ">
+        <section className="h-screen ">
             <Head title="LaraCrest - The Journey" />
             <AppNavbar />
-            <p className="text-sm mt-4 font-semibold">
-                Welcom back, {`${user.firstName} ${user.lastName}`}.
-            </p>
+            <div className="px-2 py-4 flex flex-col items-center">
+                <div className="w-[100px] h-[100px] overflow-hidden flex items-center justify-center rounded-full border-2 shadow-sm border-red-300">
+                    <img src={passport} alt="" className="w-full scale-75" />
+                </div>
+                <p className="text-sm mt-2 font-semibold">
+                    Welcome back, {user.lastName}.
+                </p>
+            </div>
         </section>
     );
 };
