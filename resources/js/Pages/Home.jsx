72311d670792376f4670594bce7d789c89dd9752
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-19 10:47:17 +0100

    testing tailwind setup. All good✅

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import {Head} from '@inertiajs/react'
+import { Head } from "@inertiajs/react";
 
-const HomePage = ()=>{
-    return (<section>
-        <Head title="LaraCrest - The Journey"/>
-        <h1>Home Page</h1></section>)
-}
+const HomePage = () => {
+    return (
+        <section className="text-center h-screen">
+            <Head title="LaraCrest - The Journey" />
+            <h1 className="font-bold text-4xl underline py-4 shadow-md">
+                Home Page
+            </h1>
+        </section>
+    );
+};
 
-export default HomePage
+export default HomePage;
