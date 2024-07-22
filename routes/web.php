a61e9156624d646f686ca9e512773a45bc45d78e
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-22 10:16:41 +0100

    redirect from root to posts route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', [HomeController::class, 'show']);
+Route::redirect('/', '/posts', 301);
+Route::get('/posts', [PostController::class, 'show']);
