24f7ec0bf8f9b8c159403e05b47ec1c0be480034
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-25 14:17:35 +0100

    added method to create posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -20,5 +21,16 @@ class PostController extends Controller
     public function store()
     {
         return Inertia::render('Post/Create');
+    }
+
+    public function createPost(Request $request)
+    {
+        $post = [
+            '_token' => $request->session()->token(),
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+
+        Post::create($post);
     }
 }
