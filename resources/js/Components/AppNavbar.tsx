3ee36827a0f4b3221819c40b5d5004ae4f1caef4
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-29 14:42:01 +0100

    added a mobile navbar

--- resources/js/Components/AppNavbar.tsx
+++ resources/js/Components/AppNavbar.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { Link } from "@inertiajs/react";
+import MobileNavbar from "./MobileNavbar";
 
 type NavbarProp = {
     loggedInUser?: unknown;
@@ -19,34 +20,37 @@ const AppNavbar: FC<NavbarProp> = ({ loggedInUser }) => {
         { name: "Newsletter", path: "/newsletter" },
     ];
     return (
-        <nav className="flex justify-between items-center py-3 px-2 h-20 sticky">
-            <div>
-                <h1 className="font-black text-black uppercase">
-                    <Link href="/">LaraCrest</Link>
-                </h1>
-            </div>
-            <ul className="flex items-center gap-2">
-                {routes.map((route) => (
-                    <li className="text-xs" key={route.name}>
-                        <Link href={route.path}>{route.name}</Link>
+        <>
+            <MobileNavbar className="md:hidden" />
+            <nav className="hidden md:flex justify-between items-center py-3 px-2 h-20 sticky">
+                <div>
+                    <h1 className="font-black text-black uppercase">
+                        <Link href="/">LaraCrest</Link>
+                    </h1>
+                </div>
+                <ul className="flex items-center gap-2">
+                    {routes.map((route) => (
+                        <li className="text-xs" key={route.name}>
+                            <Link href={route.path}>{route.name}</Link>
+                        </li>
+                    ))}
+                    <li className="flex gap-2">
+                        <Link
+                            href="/auth/register"
+                            className=" border-2 border-slate-400 py-2 px-4 text-black rounded-md uppercase text-sm font-medium leading-none flex items-center justify-center"
+                        >
+                            Sign Up
+                        </Link>
+                        <Link
+                            href="/auth/login"
+                            className="bg-slate-400 py-2 px-4 text-white rounded-md font-bold leading-none flex text-sm items-center justify-center"
+                        >
+                            Login
+                        </Link>
                     </li>
-                ))}
-                <li className="flex gap-2">
-                    <Link
-                        href="/auth/register"
-                        className=" border-2 border-slate-400 py-2 px-4 text-black rounded-md uppercase text-sm font-medium leading-none flex items-center justify-center"
-                    >
-                        Sign Up
-                    </Link>
-                    <Link
-                        href="/auth/login"
-                        className="bg-slate-400 py-2 px-4 text-white rounded-md font-bold leading-none flex text-sm items-center justify-center"
-                    >
-                        Login
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+                </ul>
+            </nav>
+        </>
     );
 };
 
---
+++ resources/js/Components/MobileNavbar.tsx
@@ -0,0 +1,80 @@
+import React, { FC, useState } from "react";
+import { Link } from "@inertiajs/react";
+import "./AppNavbar.css";
+
+type NavbarProp = {
+    loggedInUser?: unknown;
+    className?: string;
+};
+
+const MobileNavbar: FC<NavbarProp> = ({ loggedInUser, className }) => {
+    const [openNav, setOpenNav] = useState(false);
+    const routes = [
+        {
+            name: "Blog",
+            path: "/blog",
+        },
+        {
+            name: "Projects",
+            path: "/projects",
+        },
+        { name: "About", path: "/about" },
+        { name: "Newsletter", path: "/newsletter" },
+    ];
+    return (
+        <nav
+            className={`flex justify-between items-center py-3 h-20 ${className} `}
+        >
+            <div className="ms-2">
+                <h1 className="font-black text-black uppercase">
+                    <Link href="/">LaraCrest</Link>
+                </h1>
+            </div>
+            <button
+                className={`flex flex-col items-end justify-center gap-1 me-2 ${
+                    openNav ? "show" : "not-show"
+                }`}
+                onClick={() => setOpenNav(!openNav)}
+            >
+                <span className="block bg-black w-[30px] h-[3px]"></span>
+                <span className="block bg-black w-[30px] h-[3px]"></span>
+                <span className="block bg-black w-[20px] h-[3px]"></span>
+            </button>
+            <ul
+                className={`flex flex-col justify-center items-center gap-4 ${
+                    openNav ? "show-navbar" : "hide-navbar"
+                }`}
+            >
+                <h1 className="font-black text-white text-3xl mb-4 uppercase">
+                    <Link href="/">LaraCrest</Link>
+                </h1>
+
+                {routes.map((route) => (
+                    <li className="text-lg" key={route.name}>
+                        <Link href={route.path}>{route.name}</Link>
+                    </li>
+                ))}
+                <li className="flex flex-col gap-4">
+                    <Link
+                        href="/auth/register"
+                        className=" border-2 border-slate-400 py-2 px-4 text-slate-400 rounded-md uppercase text-sm font-medium leading-none flex items-center justify-center"
+                    >
+                        Sign Up
+                    </Link>
+                    <Link
+                        href="/auth/login"
+                        className="uppercase bg-slate-400 py-2 px-12 text-white rounded-md font-bold leading-none flex text-sm items-center justify-center"
+                    >
+                        Login
+                    </Link>
+                </li>
+                <button className="w-[50px] h-[50px] absolute bottom-8 rounded-full flex flex-col items-center justify-center" onClick={()=>setOpenNav(!openNav)} >
+                    <span className="bg-white min-w-[30px] min-h-[3px] block rotate-45 translate-y-[2px]"></span>
+                    <span className="bg-white min-w-[30px] min-h-[3px] block -rotate-45 -translate-y-[2px]"></span>
+                </button>
+            </ul>
+        </nav>
+    );
+};
+
+export default MobileNavbar;
