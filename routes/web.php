6e6f6b65c8d031eb6afd06d649434086b6152174
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-30 11:14:40 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterUserController;
+use App\Http\Controllers\Auth\AuthenticateUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::redirect('/', '/posts', 301);
-Route::get('/posts', [PostController::class, 'show']);
-Route::get('/posts/create', [PostController::class, 'store']);
-Route::post('/posts/create', [PostController::class, 'createPost']);
+Route::get('/posts', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/auth/register', [RegisterUserController::class, 'create'])->name('auth.create');
+Route::post('/auth/register', [RegisterUserController::class, 'store'])->name('auth.register');
+Route::get('/auth/login', [AuthenticateUserController::class, 'create']);
+Route::post('/auth/login', [AuthenticateUserController::class, 'store']);
+
+Route::get('/posts/create', [PostController::class, 'store'])->name('posts.create');
+Route::post('/posts/create', [PostController::class, 'createPost'])->name('posts.store');
+Route::get('/auth/logout', [AuthenticateUserController::class, 'logout']);
+Route::middleware(['auth'])->group(function () {
+});
