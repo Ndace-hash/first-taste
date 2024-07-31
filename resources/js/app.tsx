283eaa8267c81266955105b012eb174cd7487a9b
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-31 11:10:44 +0100

    set global title for website

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "../css/app.css";
 
 createInertiaApp({
+    title: (title) => (title ? `${title} | Laracrest` : "Laracrest"),
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.tsx", {
             eager: true,
